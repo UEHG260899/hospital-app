213663692bafe60933334f8ba1042230b30af2fb
Uriel <urielenrique99@gmail.com>
2021-05-17 20:30:02 -0500

    Se agrego el reactive form al registro

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { LoginComponent } from './login/login.component';
@@ -17,7 +17,7 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     RouterModule
   ],
   exports : [
