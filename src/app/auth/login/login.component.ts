494aab7c598f66f02496660d1ccecc27d830bd9e
Uriel <urielenrique99@gmail.com>
2021-07-05 18:31:53 -0500

    agregado : conexion login al back

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from '../../services/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -11,15 +13,30 @@ export class LoginComponent {
 
 
   public loginForm: FormGroup = this._fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required]
+    email: [ localStorage.getItem('email') || '', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    remember : [false]
   });
 
   constructor(private _router: Router,
-    private _fb: FormBuilder) { }
+    private _fb: FormBuilder,
+    private _usuarioService: UsuarioService) { }
 
   login() {
-    this._router.navigateByUrl("dashboard");
+
+    this._usuarioService.loginUsuario( this.loginForm.value )
+        .subscribe( resp => {
+          
+          if(this.loginForm.get('remember')?.value){
+            localStorage.setItem('email', this.loginForm.get('email')?.value);
+          }else{
+            localStorage.removeItem('email');
+          }
+        }, err => {
+          Swal.fire('Error', err.error.msg, 'error')
+        });
+    
+    //this._router.navigateByUrl("dashboard");
   }
 
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from "rxjs/operators";
 
 
 import { RegisterForm } from '../interfaces/register-form.interface';
+import { LoginForm } from '../interfaces/login-form.interface';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -15,6 +17,20 @@ export class UsuarioService {
   constructor(private _http: HttpClient) { }
 
   crearUsuario(formData: RegisterForm) {
-    return this._http.post(`${this._baseUrl}/usuarios`, formData);
+    return this._http.post(`${this._baseUrl}/usuarios`, formData)
+      .pipe(
+        tap((resp: any) => {
+          localStorage.setItem('token', resp.token);
+        })
+      );
+  }
+
+  loginUsuario(formData: LoginForm) {
+    return this._http.post(`${this._baseUrl}/login`, formData)
+      .pipe(
+        tap((resp: any) => {
+          localStorage.setItem('token', resp.token);
+        })
+      );
   }
 }
